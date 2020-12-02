7aba8efe4772513be5e87bcb1bca2caf90b70135
kenx00x <kenx00x@gmail.com>
2020-12-02 16:46:33 +0100

    working version 1.0.0

--- Trivia/Class1.cs
+++ Trivia/Class1.cs
@@ -1,26 +1,22 @@
 ﻿using Assets.Scripts.Simulation;
-using Assets.Scripts.Unity.UI_New;
 using Assets.Scripts.Unity.UI_New.InGame;
+using Assets.Scripts.Unity.UI_New.Main;
 using Harmony;
 using MelonLoader;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using UnityEngine;
-using System.Text;
-using System.Threading.Tasks;
-using Random = UnityEngine.Random;
-using UnityEngine.SceneManagement;
-
 [assembly: MelonInfo(typeof(Trivia.Class1), "Trivia", "1.0.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace Trivia
 {
     public class Class1 : MelonMod
     {
-        
+        public static string question = "";
+        public static string[] answers = new string[4];
+        public static string correctAnswer = "";
+        public static int staticRound;
         public override void OnApplicationStart()
         {
             MelonLogger.Log("Trivia loaded");
@@ -31,57 +27,95 @@ namespace Trivia
             input = input.Replace("&#039;", "'");
             input = input.Replace("&ldquo;", "\"");
             input = input.Replace("&rdquo;", "\"");
+            input = input.Replace("&eacute;", "é");
             return input;
         }
-        [HarmonyPatch(typeof(Simulation), "OnRoundEnd")]
-        public class SimulatornOnRoundEnd_Patch
+        public override void OnGUI()
         {
-            // Token: 0x06000003 RID: 3 RVA: 0x0000206A File Offset: 0x0000026A
+            if (question != "")
+            {
+                GUIStyle questionStyle = new GUIStyle();
+                GUIStyle buttonStyle = new GUIStyle();
+                questionStyle.alignment = TextAnchor.MiddleCenter;
+                questionStyle.normal.textColor = Color.white;
+                Texture2D blackTexture = new Texture2D(1, 1);
+                blackTexture.SetPixel(0, 0, Color.black);
+                blackTexture.Apply();
+                questionStyle.normal.background = blackTexture;
+                Texture2D grayTexture = new Texture2D(1, 1);
+                grayTexture.SetPixel(0, 0, Color.gray);
+                grayTexture.Apply();
+                buttonStyle = questionStyle;
+                buttonStyle.hover.background = grayTexture;
+                GUI.Label(new Rect(Screen.width / 2 - 400, 40, 800, 50f), question, questionStyle);
+                if (GUI.Button(new Rect(Screen.width / 2 - 400, 90, 400, 30), answers[0], buttonStyle))
+                {
+                    checkAnswer(answers[0]);
+                }
+                if (GUI.Button(new Rect(Screen.width / 2, 90, 400, 30), answers[1], buttonStyle)) 
+                {
+                    checkAnswer(answers[1]);
+                }
+                if (GUI.Button(new Rect(Screen.width / 2 - 400, 120, 400, 30), answers[2], buttonStyle)) 
+                {
+                    checkAnswer(answers[2]);
+                }
+                if (GUI.Button(new Rect(Screen.width / 2, 120, 400, 30), answers[3], buttonStyle))
+                {
+                    checkAnswer(answers[3]);
+                }
+            }
+        }
+        private void checkAnswer(string answer)
+        {
+            if (correctAnswer == answer)
+            {
+                InGame.Bridge.simulation.cashManagers.entries[0].value.cash.Value += staticRound*25;
+            }
+            for (int i = 0; i < answers.Length - 1; i++)
+            {
+                answers[i] = "";
+            }
+            question = "";
+        }
+        [HarmonyPatch(typeof(Simulation), "OnRoundEnd")]
+        public class Simulation_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(int round)
+            {
+                if (question == "")
+                {
+                    staticRound = round;
+                    System.Random rand = new System.Random();
+                    string json = new WebClient().DownloadString("https://opentdb.com/api.php?amount=1");
+                    JObject rss = JObject.Parse(json);
+                    question = ReplaceText((string)rss.SelectToken("results[0].question"));
+                    correctAnswer = ReplaceText((string)rss.SelectToken("results[0].correct_answer"));
+                    answers[3] = correctAnswer;
+                    JArray wrongAnswers = (JArray)rss.SelectToken("results[0].incorrect_answers");
+                    for (int i = 0; i < wrongAnswers.Count; i++)
+                    {
+                        wrongAnswers[i] = ReplaceText(wrongAnswers[i].ToString());
+                        answers[i] = wrongAnswers[i].ToString();
+                    }
+                    for (int i = 0; i < answers.Length - 1; i++)
+                    {
+                        int random = rand.Next(i, answers.Length);
+                        string temp = answers[i];
+                        answers[i] = answers[random];
+                        answers[random] = temp;
+                    }
+                }
+            }
+        }
+        [HarmonyPatch(typeof(MainMenu), "Open")]
+        public class MainMenu_Patch
+        {
             [HarmonyPostfix]
             public static void Postfix()
             {
-                System.Random rand = new System.Random();
-                string[] answers = new string[4];
-                string json = new WebClient().DownloadString("https://opentdb.com/api.php?amount=1");
-                JObject rss = JObject.Parse(json);
-                string question = ReplaceText((string)rss.SelectToken("results[0].question"));
-                string correctAnswer = ReplaceText((string)rss.SelectToken("results[0].correct_answer"));
-                answers[3] = correctAnswer;
-                JArray wrongAnswers = (JArray)rss.SelectToken("results[0].incorrect_answers");
-                for (int i = 0; i < wrongAnswers.Count; i++)
-                {
-                    wrongAnswers[i] = ReplaceText(wrongAnswers[i].ToString());
-                    answers[i] = wrongAnswers[i].ToString();
-                }
-                for (int i = 0; i < answers.Length - 1; i++)
-                {
-                    int random = rand.Next(i, answers.Length);
-                    string temp = answers[i];
-                    answers[i] = answers[random];
-                    answers[random] = temp;
-                }
-                GUI.Label(new Rect(100f, 160f, 100f, 50f), question);
-                /*
-                MelonLogger.Log("1");
-                GameObject test = new GameObject();
-                NK_TextMeshProUGUI questionText = test.AddComponent<NK_TextMeshProUGUI>();
-                
-                MelonLogger.Log("3");
-
-                questionText = UnityEngine.Object.Instantiate(questionText);
-                MelonLogger.Log("3");
-                RectTransform questionTransform = test.GetComponent<RectTransform>();
-                CanvasRenderer questionCanvas = test.GetComponent<CanvasRenderer>();
-                MelonLogger.Log("5");
-                SceneManager.MoveGameObjectToScene(test,SceneManager.GetSceneByName("InGameUi"));
-                MelonLogger.Log("7");
-                //questionCanvas.absoluteDepth = 3;
-                questionTransform.position = new Vector3(960, 540);
-                MelonLogger.Log("8");
-                //questionText.m_text = question;
-                MelonLogger.Log("9");
-                MelonLogger.Log("10");
-                */
+                question = "";
             }
         }
     }
